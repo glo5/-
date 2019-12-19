7860f186a35e4e3beacc253abfedd38898b29373
twins200202 <twins2002002@gmail.com>
2019-12-19 20:27:25 +0900

    Add Player

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 class App {
     constructor(){
+        App.app = this; //싱글톤을 위해 App에 스태틱으로 넣음
         this.gameWidth = 400;
         this.gameHeight = 600;
 
@@ -8,18 +9,41 @@ class App {
         this.start = false;
         this.imageList = {}; //이미지 저장 오브젝트
 
+        this.player = null;
         this.backList = []; //배경그림 리스트
+        this.playerBulletList = []; //플레이어 총알 리스트
+
         this.init(); //초기화 함수
     }
 
     async init(){
         this.imageList.player = await this.loadImage("player.png");
         this.imageList.back = await this.loadImage("back.png");
-
+        
+        //백그라운드 생성
         for(let i = 0; i < 3; i++){
-            this.backList.push(new Background(0, - i * this.gameHeight, this.gameWidth, this.gameHeight, this.imageList.back));
+            this.backList.push(
+                new Background(0, - i * this.gameHeight, 
+                                this.gameWidth, this.gameHeight,
+                                this.imageList.back));
         }
+        //플레이어 생성(x좌표 y좌표 너비 높이 이미지)
+        this.player = new Player(
+            this.gameWidth / 2 - 30, this.gameHeight - 60,
+            60, 40, this.imageList.player, this);
+
         requestAnimationFrame(this.frame.bind(this));
+    }
+
+    getOrCreateBullet(x, y, r, s, v){
+        let bullet = this.playerBulletList.find(x => !x.active);
+        if(bullet === undefined){
+            bullet = new Bullet();
+            bullet.setActive(x, y, r, s, v);
+            this.playerBulletList.push(bullet);
+        }else {
+            bullet.setActive(x, y, r, s, v);
+        }
     }
 
     loadImage(name){
@@ -43,16 +67,23 @@ class App {
 
     update(delta){
         this.backList.forEach(back => back.update(delta));
-        if(this.backList[0].y > this.gameHeight) {
+        if(this.backList[0].y > this.gameHeight){
             let first = this.backList.shift();
             first.y = this.backList[this.backList.length - 1].y - this.gameHeight;
             this.backList.push(first);
         }
+        this.player.update(delta);
+        this.player.checkOut(this.gameWidth, this.gameHeight);
+
+        this.playerBulletList.forEach(b => b.update(delta));
     }
 
     render(){
-        this.ctx.clearRect(0, 0, this.gameWidth, this.gameHeight);
+        this.ctx.clearRect(0,0,this.gameWidth, this.gameHeight);
         this.backList.forEach(back => back.render(this.ctx));
+
+        this.player.render(this.ctx);
+        this.playerBulletList.forEach(b => b.render(this.ctx));
     }
 
 }
